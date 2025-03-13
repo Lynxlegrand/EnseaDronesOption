5ceb52fd30d434dc768f0ee5cdf038e8763cdf39
Kevin Dugard <kevin.dugard@ensea.fr>
2025-03-13 08:28:47 +0100

    Merge ESC.c and Motors.c

--- Software/STM32/Main/drone_main_code/Core/Inc/ESC.h
+++ Software/STM32/Main/drone_main_code/Core/Inc/ESC.h
@@ -1,7 +1,23 @@
+#ifndef INC_MOTOR_H_
+#define INC_MOTOR_H_
 
+#include "main.h"
 
+typedef struct {
+    TIM_HandleTypeDef* htim;
+    uint32_t channel;
+    float PercentageOfTotalPower;
+} h_motor_t;
+
+int percentageToMicrosecondsAtHighState(int percentage);
+void motor_Init(h_motor_t* h_motor);
+void motor_SetPower(h_motor_t* h_motor, int percentage);
+void motor_TurnOff(h_motor_t* h_motor);
 
 #define MOTOR_FRONT_LEFT 0
 #define MOTOR_FRONT_RIGHT 1
 #define MOTOR_BACK_LEFT 2
 #define MOTOR_BACK_RIGHT 3
+
+#endif
+
