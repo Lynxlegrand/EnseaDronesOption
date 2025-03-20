b9198bb67a585a9ff4fa81a31f223d57126c06e1
Kevin Dugard <kevin.dugard@ensea.fr>
2025-03-20 11:45:11 +0100

    Updated motors code

---
+++ Software/STM32/Peripherals/Motors/Core/Inc/motor.h
@@ -0,0 +1,18 @@
+#ifndef INC_MOTOR_H_
+#define INC_MOTOR_H_
+
+#include "main.h"
+
+// Motors management
+typedef struct {
+	TIM_HandleTypeDef* htim;
+	uint32_t channel;
+	float PercentageOfTotalPower;
+} h_motor_t;
+
+int percentageToMicrosecondsAtHighState(int percentage);
+void motor_Init(h_motor_t* h_motor);
+void motor_SetPower(h_motor_t* h_motor, int percentage);
+void motor_TurnOff(h_motor_t* h_motor);
+
+#endif /* INC_MOTOR_H_ */
